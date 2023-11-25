0eb0570687371318e20405788dae533e69297c8f
Chandan-DK <chandandk468@gmail.com>
2023-11-25 19:36:07 +0000

    add tag genclient and make codegen-client-clientset
    Signed-off-by: Chandan-DK <chandandk468@gmail.com>

--- api/mygroup/v1alpha1/types.go
+++ api/mygroup/v1alpha1/types.go
@@ -4,6 +4,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // PodCreator describes a PodCreator custom resource
 type PodCreator struct {
