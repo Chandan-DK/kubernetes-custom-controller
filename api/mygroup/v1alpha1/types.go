0fd414e2d24c6fc0917de02a0535d2b1a9bc6d90
Chandan-DK <chandandk468@gmail.com>
2023-11-25 19:01:32 +0000

    remove const from types as we are generating register.go
    Signed-off-by: Chandan-DK <chandandk468@gmail.com>

--- api/mygroup/v1alpha1/types.go
+++ api/mygroup/v1alpha1/types.go
@@ -4,17 +4,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// These const variables are used in our custom controller.
-const (
-	GroupName string = "mygroup"
-	Version   string = "v1alpha1"
-	Kind      string = "PodCreator"
-	Singluar  string = "podcreator"
-	Plural    string = "podcreators"
-	ShortName string = "pcr"
-	Name      string = Plural + "." + GroupName
-)
-
 type PodCreator struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
