c1b4a858d3e71a44ce87c42ceb63958107021e2e
Chandan-DK <chandandk468@gmail.com>
2023-11-25 19:06:50 +0000

    add tags to types so that DeepCopyObject() from runtime.Object interface can be generated by deepcopy-gen
    Signed-off-by: Chandan-DK <chandandk468@gmail.com>

--- api/mygroup/v1alpha1/types.go
+++ api/mygroup/v1alpha1/types.go
@@ -4,6 +4,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// PodCreator describes a PodCreator custom resource
 type PodCreator struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -16,6 +18,8 @@ type PodCreatorSpec struct {
 	Labels map[string]string `json:"labels"`
 }
 
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// PodCreatorList is a list of PodCreator resources.
 type PodCreatorList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
